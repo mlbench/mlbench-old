be8773615f239bd45119b0f05fc36666f9a791b7
amirreza <amireza.bahreini@gmail.com>
2016-03-14 12:52:47 +0100

    hing loss implemented

--- dml-scala/src/main/scala-2.11/Functions.scala
+++ dml-scala/src/main/scala-2.11/Functions.scala
@@ -17,6 +17,13 @@ object Functions {
     return 0.5 * w.dot(w);
   }
 
+  def hingeLoss(w: DenseVector[Double], xi:DenseVector[Double], yi: Double): Double ={
+    //Change labels from 0,1 to -1,1
+    val y:Double = 2 * yi - 1
+    return max(0, 1 - y * w.dot(xi))
+
+  }
+
   def none_reg(w: DenseVector[Double]): Double ={
     return 0;
   }
