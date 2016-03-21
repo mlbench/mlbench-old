7125d402953f3ccfb53fb49b6eda9603f7292ee6
amirreza <amireza.bahreini@gmail.com>
2016-03-21 11:22:51 +0100

    Regularization term is now computed in a seperate manner

--- dml-scala/src/main/scala-2.11/LogisticRegression.scala
+++ dml-scala/src/main/scala-2.11/LogisticRegression.scala
@@ -1,36 +1,36 @@
 import Functions._
-import breeze.linalg.{DenseVector, min}
-import breeze.numerics.sqrt
+import breeze.linalg.{DenseVector}
+import breeze.numerics.{sqrt}
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
 
 /**
   * Created by amirreza on 09/03/16.
   */
-class LogisticRegression(data: RDD[LabeledPoint],
-                         //No regularizer term by default:
-                         regularizer: Regularizer = new Unregularized,
+class LogisticRegression(regularizer: Regularizer = new Unregularized,//No regularizer term by default:
                          lambda: Double = 0.0,
                          iterations: Int = 100,
-                         stepSize : Double = 1.0) {
+                         stepSize : Double = 1.0) extends Serializable{
+  val loss = new BinaryLogistic
   var gamma:Double = stepSize
 
-  def train(): DenseVector[Double] ={
+  def train(data: RDD[LabeledPoint]): DenseVector[Double] ={
     // Initialize w to zero
     val d : Int = data.first().features.size
     val n : Double = data.count()
     var w : DenseVector[Double] = DenseVector.fill(d){0.0}
-    val loss:LossFunction = new BinaryLogistic
 
     val eval = new Evaluation(loss, regularizer, lambda)
     for (i <- 1 to iterations) {
       gamma = stepSize / sqrt(iterations)
-      val gradient = data.map { p =>
+
+      val loss_gradient = data.map { p =>
         loss.subgradient(w, DenseVector(p.features.toArray), p.label)
       }.reduce(_ + _)
-      w -= gamma * (gradient + lambda * regularizer.subgradient(w) * n)
+      val reg_gradient = data.map(_ => regularizer.subgradient(w)).reduce(_ + _)
+
+      w -= gamma * (loss_gradient + lambda * reg_gradient)
     }
-    println("Logistic w: " + w)
     return w;
   }
 }
--- dml-scala/src/main/scala-2.11/SVM.scala
+++ dml-scala/src/main/scala-2.11/SVM.scala
@@ -7,31 +7,30 @@ import org.apache.spark.rdd.RDD
 /**
   * Created by amirreza on 09/03/16.
   */
-class SVM(data: RDD[LabeledPoint],
-          //No regularizer term by default:
-          regularizer: Regularizer = new Unregularized,
+class SVM(regularizer: Regularizer = new Unregularized,//No regularizer term by default:
           lambda: Double = 0.0,
           iterations: Int = 100,
-          stepSize : Double = 1.0) {
-
+          stepSize : Double = 1.0) extends Serializable {
+  val loss = new HingeLoss
   var gamma: Double = stepSize
 
-  def train(): DenseVector[Double] ={
+  def train(data: RDD[LabeledPoint]): DenseVector[Double] ={
     // Initialize w to zero
     val d : Int = data.first().features.size
     val n : Double = data.count()
     var w : DenseVector[Double] = DenseVector.fill(d){0.0}
-    val loss:LossFunction = new HingeLoss
 
     val eval = new Evaluation(loss = loss, lambda = lambda, regularizer = regularizer)
     for (i <- 1 to iterations) {
       gamma = stepSize / sqrt(iterations)
-      val gradient = data.map { p =>
+
+      val loss_gradient = data.map { p =>
         loss.subgradient(w, DenseVector(p.features.toArray), p.label)
       }.reduce(_ + _)
-      w -= gamma * (gradient + lambda * regularizer.subgradient(w) * n)
+      val reg_gradient = data.map{ _ => regularizer.subgradient(w)}.reduce(_ + _)
+
+      w -= gamma * (loss_gradient + lambda * reg_gradient)
     }
-    println("SVM w: " + w)
     return w;
   }
 }
