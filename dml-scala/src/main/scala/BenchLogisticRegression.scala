e49a00f20626826ea2841c789eea369ffbc3bd77
Marcel Schoengens <mschoengens@bitvalve.org>
2016-04-06 09:42:19 +0200

    Added a repartitioning of the input data which is controllable by a clp.

--- dml-scala/src/main/scala/BenchLogisticRegression.scala
+++ dml-scala/src/main/scala/BenchLogisticRegression.scala
@@ -7,7 +7,7 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.rdd.RDD
-
+import org.apache.spark.util.SizeEstimator
 import org.apache.spark._
 
 
@@ -15,8 +15,12 @@ object BenchLogisticRegression {
 
   def main(args: Array[String]) {
     var file = ""
-    if (args.length > 0)
+    var parts = 2
+
+    if (args.length > 0) {
       file = args(0)
+      parts = args(1).toInt
+    }
 
     //Spark conf
     val conf = new SparkConf().setAppName("Distributed Machine Learning").setMaster("local[*]")
@@ -29,16 +33,18 @@ object BenchLogisticRegression {
     rootLogger.setLevel(Level.ERROR)
 
     //Load data
-    val data: RDD[LabeledPoint] = MLUtils.loadLibSVMFile(sc, file)
+    val rawData: RDD[LabeledPoint] = MLUtils.loadLibSVMFile(sc, file)
+    val data = rawData.repartition(parts)
+    
 
     //Set optimizer's parameters
     val params = new Parameters(
       stepSize = 0.1,
-      iterations = 100,
+      iterations = 2,
       lambda = 0.1
     )
-    val reg = new L2Regularizer
 
+    val reg = new L2Regularizer
     val lr = new LogisticRegression(regularizer = reg, params)
 
     val start = System.nanoTime()
@@ -49,6 +55,8 @@ object BenchLogisticRegression {
     println("Logistic w: " + w1)
     println("Logistic Objective value: " + objective1)
     println("Training took: " + elap / 1000 / 1000 + "ms")
+    println("Number of partitions: " + data.partitions.size)
+    println("Size of data rdd: " + SizeEstimator.estimate(data) + " bytes.")
     println("----------------------------")
   }
 }
