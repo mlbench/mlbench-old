baa67de0b1133ec1cd16101da3b899fa2514ae68
Nooshin Mirzadeh <mirzadeh@parsapc15.epfl.ch>
2017-06-14 19:24:52 +0200

    Add the parameters and update the run.sh

--- bench-spark/run.sh
+++ bench-spark/run.sh
@@ -23,20 +23,19 @@ shift
 PARTITIONS=$1
 shift
 
-mkdir /data/results
 #/bin/bash
 echo "********************************************************************************************************Dividing the dataset************************************************************"
 ${SPARK_HOME}/bin/spark-submit --class "PrepareData" "$@" ${BENCHMARK_DIR}/target/scala-2.11/bench-spark-assembly-1.0.jar \
-       -d $DATASET -w "/data/results/" -m $METHOD -p $PARTITIONS 
+       -d $DATASET -w "/data/" -m $METHOD -p $PARTITIONS 
 echo "********************************************************************************************************Running the benchmark***********************************************************"
 if [ $METHOD = "Regression" ]; then
 	${SPARK_HOME}/bin/spark-submit --class "RUN" "$@" ${BENCHMARK_DIR}/target/scala-2.11/bench-spark-assembly-1.0.jar \
-        	-w "/data/results/" L1_Lasso_GD L1_Lasso_SGD Mllib_Lasso_SGD Mllib_Lasso_GD
+        	-w "/data/" L1_Lasso_GD L1_Lasso_SGD Mllib_Lasso_SGD Mllib_Lasso_GD
 else
 	${SPARK_HOME}/bin/spark-submit --class "RUN" "$@" ${BENCHMARK_DIR}/target/scala-2.11/bench-spark-assembly-1.0.jar \
-		-w "/data/results/" L2_SVM_SGD L2_SVM_GD L2_LR_SGD L2_LR_GD L1_LR_SGD L1_LR_GD Mllib_L1_LR_SGD Mllib_L2_LR_SGD Mllib_L2_SVM_SGD Mllib_L2_SVM_GD 
+		-w "/data/" L2_SVM_SGD L2_SVM_GD L2_LR_SGD L2_LR_GD L1_LR_SGD L1_LR_GD Mllib_L1_LR_SGD Mllib_L2_LR_SGD Mllib_L2_SVM_SGD Mllib_L2_SVM_GD 
 fi
 echo "********************************************************************************************************Running the evaluation***********************************************************"
-${SPARK_HOME}/bin/spark-submit --class "Evaluate" "$@" ${BENCHMARK_DIR}/target/scala-2.11/bench-spark-assembly-1.0.jar -d "/data/results/"
+${SPARK_HOME}/bin/spark-submit --class "Evaluate" "$@" ${BENCHMARK_DIR}/target/scala-2.11/bench-spark-assembly-1.0.jar -d "/data/"
  
 bash
