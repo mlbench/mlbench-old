7e235b994fc12b6799c375a3464f9766ee8caf38
Ralf Grubenmann <ralf.grubenmann@students.fhnw.ch>
2018-06-28 11:41:42 +0200

    Initial commit for new version, removes old code

--- legacy-bench-spark/demo-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-spark-submit --class "PrepareData" target/scala-2.10/bench-spark-assembly-1.0.jar -d ../datasets/breast-cancer_scale.libsvm -m Regression
-
-spark-submit --class "RUN" target/scala-2.10/bench-spark-assembly-1.0.jar L1_Lasso_SGD Mllib_Lasso_SGD L1_Lasso_ProxCocoa Elastic_ProxCocoa
-
-spark-submit --class "Evaluate" target/scala-2.10/bench-spark-assembly-1.0.jar
-
