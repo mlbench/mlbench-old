5aa144a187e4a563304688eb3f21700dea4f68b7
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2018-07-25 18:25:58 +0200

    Adds metrics endpoint Adds working prometheus

--- mlbench/master/api/apps.py
+++ mlbench/master/api/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
+from redis import Redis
+from rq import Queue
+from rq_scheduler import Scheduler
+from datetime import datetime
 
 
 class ApiConfig(AppConfig):
     name = 'api'
+
+    def ready(self):
+        scheduler = Scheduler(connection=Redis())
+
+        scheduler.schedule(scheduled_time=datetime.utcnow(), func=..., interval=1, repeat=None)
+
