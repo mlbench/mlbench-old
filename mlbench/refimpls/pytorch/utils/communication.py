30301c583137bc716870cd70411cfc03304431c2
Negar Foroutan <n.forootan.en@gmail.com>
2018-10-22 16:36:09 +0200

    Add a new aggregate function to handle sparsifiedSGD

--- mlbench/refimpls/pytorch/utils/communication.py
+++ mlbench/refimpls/pytorch/utils/communication.py
@@ -16,10 +16,32 @@ def aggregate_gradients(model, world_size):
         param.grad.data /= world_size
 
 
+def aggregate_sparsified_gradients(model, world_size, sparse_vector_size, random_sparse, optimizer, lr):
+    """Make the gradients vector sparse and average sparsified gradients of models across all processes."""
+
+    params_sparse_tensors = optimizer.sparsify_gradients(model, lr, random_sparse)
+
+    for ind, param in enumerate(model.parameters()):
+
+        gathered_list = [torch.zeros_like(params_sparse_tensors[ind]) for _ in range(world_size)]
+        # all gather.
+        dist.all_gather(gathered_list, params_sparse_tensors[ind])
+
+        avg_grads = torch.zeros_like(param.data)
+
+        for grad_tensor in gathered_list:
+            begin = int(grad_tensor[0, sparse_vector_size])
+            avg_grads[0, begin:begin + sparse_vector_size] += grad_tensor[0, 0:sparse_vector_size]
+
+        avg_grads /= world_size
+        param.grad.data = avg_grads
+
+
 def global_average(sum, count):
     def helper(array):
         array = torch.FloatTensor(array)
         dist.all_reduce(array, op=dist.reduce_op.SUM)
         return array[0] / array[1]
+
     avg = helper([sum, count])
     return avg
